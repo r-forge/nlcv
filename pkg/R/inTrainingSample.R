8bcc13d53a90cd8d04c140386a4f4af53feb5760
tobias_verbeke (Tobias Verbeke) <tobias.verbeke@telenet.be>
2009-01-13 22:22:30 +0000

    added distclass argument (for sampling in case of unbalanced classes)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nlcv@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/inTrainingSample.R
+++ pkg/R/inTrainingSample.R
@@ -1,8 +1,12 @@
 ### make random partitions
 # returns a logical vector indicating whether the observation
 # 
-inTrainingSample <- function(y, propTraining = 2/3) # was: RandomPartition
+inTrainingSample <- function(y, propTraining = 2/3, classdist = c("balanced", "unbalanced")) # was: RandomPartition
 {
+  classdist <- match.arg(classdist)
+  if (!(classdist %in% c("balanced", "unbalanced")))
+    stop("'classdist' should be one of 'balanced' or 'unbalanced'")
+  
   # sample sizes
   nTotalSample <- length(y)
   nTrainingSample <- round(propTraining * nTotalSample)
@@ -11,18 +15,46 @@ inTrainingSample <- function(y, propTraining = 2/3) # was: RandomPartition
   if (nlevels(factor(y) != 2))
     stop("'nlcv' currently only works for two-class problems")
   
-  K <- nlevels(factor(y))
-  LearnSampRun <- NULL
-  props    <- round(nTrainingSample / nTotalSample * table(y))
-  props[1] <- nTrainingSample - sum(props[2:K])
-  
-  for (k in 1:K){
-    y.num  <- as.numeric(factor(y))
-    LearnSampRun <- c(LearnSampRun, sample(which(y.num == k))[1:props[k]])
+  if (classdist == "balanced"){ 
+    
+    K <- nlevels(factor(y))
+    trainingSampleRun <- NULL
+    props    <- round(nTrainingSample / nTotalSample * table(y))
+    props[1] <- nTrainingSample - sum(props[2:K])
+    
+    for (k in 1:K){
+      y.num  <- as.numeric(factor(y))
+      trainingSampleRun <- c(trainingSampleRun, sample(which(y.num == k))[1:props[k]])
+    }
+    
+    res <- rep(FALSE, length = nTotalSample)
+    res[trainingSampleRun] <- TRUE 
+  } else {
+    
+    smallestClass <- names(sort(table(y)))[1]
+    nSmallest <- sum(y == smallestClass)
+   
+    nSmallestTrain <- round(propTraining * nSmallest)
+    nBiggestTrain <- nSmallestTrain
+    nSmallestTest <- nSmallest - nSmallestTrain
+    nBiggestTest <- nTotalSample - (nSmallestTest + nSmallestTrain + nBiggestTrain)
+    
+    
+    # split up in smallest class indices and biggest class indices
+    smallestIndices <- which(y == smallestClass)
+    biggestIndices <- seq(along = y)[-smallestIndices]
+    
+    sampleSmallestTrain <- sample(smallestIndices, nSmallestTrain)
+    # sampleSmallestTest <- smallestIndices[-sampleSmallestTrain]
+    
+    sampleBiggestTrain <- sample(biggestIndices, nBiggestTrain)
+    # sampleBiggestTest <- biggestIndices[-sampleBiggestTrain]
+    
+    trainingSampleRun <- c(sampleSmallestTrain, sampleBiggestTrain)
+    res <- rep(FALSE, length = nTotalSample)
+    res[trainingSampleRun] <- TRUE
+    
   }
-  
-  res <- rep(FALSE, length = nTotalSample)
-  res[LearnSampRun] <- TRUE 
   return(res)
 }
 
