db87f8df5f959212f442337bff2508850c888e04
tobias_verbeke (Tobias Verbeke) <tobias.verbeke@telenet.be>
2009-03-04 17:04:50 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nlcv@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- nlcv/R/xtable.summary.mcrPlot.R
+++
@@ -1,7 +0,0 @@
-xtable.summary.mcrPlot <- function(x, caption = NULL, label = NULL, align = NULL, 
-    digits = NULL, display = NULL, ...){
-  smpMat <- as.matrix(unclass(x))
-  xtable(smpMat, caption = caption, label = label, align = align, 
-         digits = digits, display = display, ...)
-}
-
--- nlcv/R/zzz.R
+++
@@ -1,5 +0,0 @@
-.onAttach <- function(libname, pkgname){
-  options(error = NULL)
-  message(paste("\nnlcv version ", packageDescription("nlcv")$Version, 
-          "\n", sep = ""))
-}
